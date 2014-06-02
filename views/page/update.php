e5a78b9619ea6999c59a506c3663d3298bc58a69
Philipp Frenzel <philipp@frenzel.net>
2014-06-02 19:45:27 +0200

    enhancing the cmd

--- views/page/update.php
+++ views/page/update.php
@@ -1,6 +1,8 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
+use kartik\widgets\SideNav;
 
 /**
  * @var yii\base\View $this
@@ -12,6 +14,27 @@ $this->params['breadcrumbs'][] = array('label' => 'Pages', 'url' => array('index
 $this->params['breadcrumbs'][] = array('label' => $model->name, 'url' => array('view', 'id' => $model->id));
 $this->params['breadcrumbs'][] = 'Update';
 ?>
+
+<?php yii\widgets\Block::begin(array('id'=>'sidebar')); ?>
+
+  <?php 
+
+    $sideMenu = array();
+    $sideMenu[] = array('icon'=>'arrow-left','label'=>Yii::t('app','Overview'),'url'=>Url::to(array('/pages/page/index')));
+    //$sideMenu[] = array('icon'=>'plus','label'=>Yii::t('app','New Page'),'url'=>Url::to(array('/pages/page/create')));
+    //$sideMenu[] = array('icon'=>'arrow-right','label'=>Yii::t('app','Manage Categories'),'url'=>Url::to(array('/categories/categories/index')));
+    //$sideMenu[] = array('icon'=>'arrow-right','label'=>Yii::t('app','Manage Tags'),'url'=>Url::to(array('/tags/default/index')));
+   
+    echo SideNav::widget([
+      'type' => SideNav::TYPE_INFO,
+      'heading' => 'CMS Menu',
+      'items' => $sideMenu
+    ]);
+
+  ?>
+
+<?php yii\widgets\Block::end(); ?>
+
 <div class="module-wsp">
 
 	<h1><?= Html::encode($this->title); ?></h1>
